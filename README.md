98620a82aa23fcdccf3971adef719126b9d7bcb6
Rui <111970306+Rui-MartinsDev@users.noreply.github.com>
2024-01-07 18:10:27 +0000

    New updates (1.1 and 1.2)

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Bouncing-ball
 Simple game to hit the ball
+
+Stories:
+1. Make the ball appear in random places throughout the window
+2. Add a slider to make the ball go faster or slower
+3. Create a countdown after the countdown show the score, and keep the score for a record,
+4. Add a button to restart the game
+/* */
+5. Make a button to go in a separate page, for a diferent game.
+6. This game is a ball but instead of moving, it stays still and disappears after some time, and needs to be clicked to earn the point
+
+Updates:
+1.01- Now the ball changes to any hexdecimal color.
+1.1 - The ball now appears randomly in the window
+1.2 - Added a slider for the speed
--- index.js
+++ index.js
@@ -1,28 +1,43 @@
-let ball = document.querySelector('div');
-ball.addEventListener('mouseover', move);
-let title =document.querySelector('#h1');
+let ball = document.querySelector('#div');  //the ball
+ball.addEventListener('mouseover', move);   //updates the ball
+let title =document.querySelector('#h1');   //shows the score
+
 let timerId = null;
 let ballWidth=ball.offsetWidth, ballHeight=ball.offsetHeight;
 let hits=-1;
 let windowWidth,windowHeight;
+let colors= [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+let x=0, y=0;
+var slider = document.getElementById("slider");
 
+function updateSliderValue() { //for the slider
+    var sliderValueElement = document.getElementById("sliderValue");
+    sliderValueElement.textContent = slider.value;
+}
 
-
-function generateRandomNumber() { //doesnt generate all colors (doenst includeA-F)
-    const randomFraction = Math.random(); 
-    const randomNumber = Math.floor(randomFraction * 999999) + 1;
-    const formattedNumber = randomNumber.toString().padStart(6, '0');
-    return formattedNumber;
+function generateRandomColor() { // generates all colors
+    let randomColor='';
+    for (let i=0;i<6;i++){
+        randomColor+= colors[Math.floor(Math.random()*colors.length)]
+    }
+    return randomColor;
   }
-const speed = () => Math.random()*5+1; //returns a random number to make up for the speed
+
+const speed = () => Math.random()*slider.value+1; //set the ball speed by returning a random number
+
+function generateRandomPosition(){
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+    x=Math.floor(Math.random()*(windowWidth-ballWidth))
+    y=Math.floor(Math.random()*(windowHeight-ballHeight-60))+60
+}
 
 function move(){
     
     if (timerId) { //if the frame func was already going, stops it
         clearInterval(timerId);
     }
-    let x=0; //starts here
-    let y=60;
+    generateRandomPosition();
     let xD=speed();
     let yD=speed();
 
@@ -33,15 +48,15 @@ function move(){
         windowHeight = window.innerHeight;
         if (x>=windowWidth-ballWidth || x<0){ //touching the edges of the screen:
             xD *=-1;                    //inverts the movement
-            ball.style.background ='#'+generateRandomNumber(); //changes the color
+            ball.style.background ='#'+generateRandomColor(); //changes the color
             hits--; //removes 1 point!
-            title.textContent=`Ball hits: ${hits}`;
+            title.textContent=`Score: ${hits}`;
         } 
         if (y>=windowHeight - ballHeight || y<60) {
             yD*=-1;
-            ball.style.background ='#'+generateRandomNumber();
+            ball.style.background ='#'+generateRandomColor();
             hits--;
-            title.textContent=`Ball hits: ${hits}`;
+            title.textContent=`Score: ${hits}`;
         }
             x+=1 *xD; //updates the coords every reiteration
             y+=1 * yD;
@@ -51,7 +66,8 @@ function move(){
     }
     ballHits();
 }
-function ballHits(){
+
+function ballHits(){ //updates the score on hit
     hits++;
-    title.textContent=`Ball hits: ${hits}`;
+    title.textContent=`Score: ${hits}`;
 }
